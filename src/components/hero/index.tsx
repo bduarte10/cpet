21f9fa9907be5cbf681529aba73b4bd608013a78
Pedro Santos <phsantosti@gmail.com>
2024-04-05 17:35:13 -0300

    alterar link de pagina de venda bolsa mais brasil para pagina de venda embaixadores do saber, mudar a rota para o envio de formulario de email

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -30,7 +30,8 @@ export const Hero = () => {
 
     try {
       setSending(true)
-      await axios.post('/send-email', data)
+      //await axios.post('/send-email', data)
+      await axios.post('/lead/email/send', data)
       setNome('')
       setEmail('')
       setCelular('')
