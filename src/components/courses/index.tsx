41a351095e44558deef5d8b0135bdfd5bd204328
Bruno <duartebruno2208@gmail.com>
2024-04-28 18:05:20 -0300

    fix

--- src/components/courses/index.tsx
+++ src/components/courses/index.tsx
@@ -1,37 +1,36 @@
-import { useCourses } from '@/hooks/useCourses'
-import { Course } from '../course'
-import { SkeletonCard } from '../skeletonCard'
+import { useCourses } from "@/hooks/useCourses";
+import { SkeletonCard } from "../skeletonCard";
+import { Course } from "../Course";
 
 interface ICourse {
-  id: string
-  name: string
-  type: string
-  duration: string
-  duration_type: string
-  discount_percent: string
+  id: string;
+  name: string;
+  type: string;
+  duration: string;
+  duration_type: string;
+  discount_percent: string;
 }
 export const Courses = () => {
-  const { data: courses, isLoading } = useCourses()
+  const { data: courses, isLoading } = useCourses();
 
-  const excludedIds = new Set(['7287', '7288', '7286', '4804'])
-  const memo = new Map()
+  const excludedIds = new Set(["7287", "7288", "7286", "4804"]);
+  const memo = new Map();
 
   const filtered = courses?.filter((course: any) => {
     if (memo.has(course.id)) {
-      return !memo.get(course.id)
+      return !memo.get(course.id);
     }
 
-    const excluded = excludedIds.has(course.id)
-    memo.set(course.id, excluded)
-    return !excluded
-  })
+    const excluded = excludedIds.has(course.id);
+    memo.set(course.id, excluded);
+    return !excluded;
+  });
 
   return (
     <section id="cursos" className="w-full bg-slate-100">
       <div className="max-w-7xl py-14 px-10 lg:px-0 lg:mx-auto">
         <h2 className="max-w-2xl mx-auto text-2xl mb-14 font-extrabold text-center text-slate-900">
-          Confira os Cursos Disponíveis no EAD da{' '}
-          <span className="text-2xl text-sky-700">CPET</span> <br />A MAIOR
+          Confira os Cursos Disponíveis no EAD da <span className="text-2xl text-sky-700">CPET</span> <br />A MAIOR
           ESCOLA TÉCNICA DO BRASIL
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
@@ -42,5 +41,5 @@ export const Courses = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
--- src/pages/Curso.tsx
+++ src/pages/Curso.tsx
@@ -1,80 +1,76 @@
-import { Button } from '@/components/ui/button'
-import { Loader } from '@/components/ui/loader'
-import { useCourseById } from '@/hooks/useCourseById'
-import { Link, useParams } from 'react-router-dom'
-import axios from 'axios'
+import { Button } from "@/components/ui/button";
+import { Loader } from "@/components/ui/loader";
+import { useCourseById } from "@/hooks/useCourseById";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
-import { formatCurrency } from '@/utils/functions'
-import { Clock, GraduationCap, Laptop, Trophy } from 'lucide-react'
-import { Separator } from '@/components/ui/separator'
-import { courseData } from '@/constants/courses'
-import { Input } from '@/components/ui/input'
-import DynamicAccordion from '@/components/dinamicAccordion'
-import { useEffect, useRef, useState } from 'react'
-import Selo from '@/assets/selo.png'
+import { formatCurrency } from "@/utils/functions";
+import { Clock, GraduationCap, Laptop, Trophy } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { courseData } from "@/constants/courses";
+import { Input } from "@/components/ui/input";
+import DynamicAccordion from "@/components/dinamicAccordion";
+import { useEffect, useRef, useState } from "react";
+import Selo from "@/assets/selo.png";
 interface FormData {
-  nome: string
-  email: string
-  celular: string
+  nome: string;
+  email: string;
+  celular: string;
 }
 
 export const Curso = () => {
-  let { id } = useParams()
-  const { data, isLoading } = useCourseById(Number(id))
+  let { id } = useParams();
+  const { data, isLoading } = useCourseById(Number(id));
 
-  const [nome, setNome] = useState('')
-  const [email, setEmail] = useState('')
-  const [celular, setCelular] = useState('')
-  const [sending, setSending] = useState(false)
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [celular, setCelular] = useState("");
+  const [sending, setSending] = useState(false);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault() // Evite o comportamento padrão de envio do formulário
+    event.preventDefault(); // Evite o comportamento padrão de envio do formulário
 
     // Construa o objeto de dados a serem enviados
     const formData: FormData = {
       nome: nome,
       email: email,
       celular: celular,
-    }
+    };
 
     try {
-      const course = data?.[0];
-
       setSending(true);
 
-      //const response = await axios.post('/send-email', data)
-      const response = await axios.post('/lead/email/send', formData);
-      console.log(response.data); // Exibe a resposta do servidor no console
+      const response = await axios.post("/lead/email/send", formData);
+      console.log(response.data);
 
-      // Limpe os campos do formulário após o envio bem-sucedido
-      setNome('');
-      setEmail('');
-      setCelular('');
+      setNome("");
+      setEmail("");
+      setCelular("");
       setSending(false);
     } catch (error) {
-      console.error('Erro ao enviar e-mail:', error)
-      // Trate o erro, se necessário
+      console.error("Erro ao enviar e-mail:", error);
     } finally {
       setTimeout(() => {
-        window.open(`${course?.pagina_venda_embaixador}?studant_name=${formData.nome}&studant_email=${formData.email}&studant_cell_phone=${formData.celular}`, '_blank')
+        window.open(
+          `${course?.pagina_venda_embaixador}?studant_name=${formData.nome}&studant_email=${formData.email}&studant_cell_phone=${formData.celular}`,
+          "_blank"
+        );
       }, 2000);
     }
-  }
+  };
 
-  const videoUrl = courseData.find((course) => course.id === id)?.videoUrl
-  const dataAccordion = courseData.find(
-    (course) => course.id === id,
-  )?.curriculum
+  const videoUrl = courseData.find((course) => course.id === id)?.videoUrl;
+  const dataAccordion = courseData.find((course) => course.id === id)?.curriculum;
 
-  const course = data?.[0]
+  const course = data?.[0];
 
-  const topRef = useRef<HTMLDivElement | null>(null)
+  const topRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (topRef.current) {
-      topRef.current.scrollIntoView({ behavior: 'smooth' })
+      topRef.current.scrollIntoView({ behavior: "smooth" });
     }
-  }, [])
+  }, []);
 
   return (
     <>
@@ -82,19 +78,16 @@ export const Curso = () => {
         {isLoading && <Loader />}
         <div className=" pt-24 pb-14 w-full flex items-center mx-auto px-10 bg-sky-800">
           <div className="max-w-7xl w-full mx-auto  flex flex-start flex-col">
-            <h1 className="text-4xl w-4/5 text-balance mb-2 text-yellow-300 font-extrabold">
-              {course?.course}
-            </h1>
+            <h1 className="text-4xl w-4/5 text-balance mb-2 text-yellow-300 font-extrabold">{course?.course}</h1>
             <p className="text-lg italic text-balance font-medium text-white">
               Prepare-se para oportunidades de carreira imperdíveis!
             </p>
             <Button
               asChild
-              variant={'outline'}
-              className="mt-4 w-fit h-14 text-md text-sky-800 bg-yellow-300 hover:bg-yellow-400">
-              <Link
-                className="font-bold"
-                to={course?.pagina_venda_embaixador}>
+              variant={"outline"}
+              className="mt-4 w-fit h-14 text-md text-sky-800 bg-yellow-300 hover:bg-yellow-400"
+            >
+              <Link className="font-bold" to={course?.pagina_venda_embaixador}>
                 GARANTA AGORA!
               </Link>
             </Button>
@@ -108,16 +101,16 @@ export const Curso = () => {
                 src={`https://www.youtube.com/embed/${videoUrl}`}
                 title={course?.course}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen></iframe>
+                allowFullScreen
+              ></iframe>
             )}
             <div className="mb-8 mt-4">
               <h2 className="text-3xl text-balance mb-2  font-bold leading-relaxed text-sky-800">
                 Suporte e Acompanhamento
               </h2>
               <p className="text-lg text-balance font-normal text-slate-600 pr-2">
-                Durante todo o curso, você contará com o suporte de nossa equipe
-                de tutores, prontos para auxiliá-lo em seu desenvolvimento
-                acadêmico.
+                Durante todo o curso, você contará com o suporte de nossa equipe de tutores, prontos para auxiliá-lo em
+                seu desenvolvimento acadêmico.
               </p>
             </div>
             <div className="mb-5">
@@ -125,8 +118,8 @@ export const Curso = () => {
                 Conheça a Grade Curricular do Curso
               </h2>
               <p className="text-lg text-balance font-normal text-slate-600 pr-2">
-                Prepare-se para adquirir os conhecimentos teóricos e práticos
-                necessários para se destacar nessa área em constante evolução.
+                Prepare-se para adquirir os conhecimentos teóricos e práticos necessários para se destacar nessa área em
+                constante evolução.
               </p>
             </div>
             <div className="w-full">
@@ -135,29 +128,22 @@ export const Curso = () => {
           </div>
           <div className="max-w-96 space-y-1 shadow-xl p-4 flex flex-col  items-center bg-white rounded-sm ">
             <div className=" relative p-5 space-y-2 flex flex-col w-full rounded-sm">
-              <img
-                src={Selo}
-                alt="Selo de qualidade"
-                className="absolute top-0 right-0 w-16 md:w-28 "
-              />
+              <img src={Selo} alt="Selo de qualidade" className="absolute top-0 right-0 w-16 md:w-28 " />
               <p className="text-sm text-foreground line-through font-medium">
                 {course?.original_installments}x KTR
                 {formatCurrency(Number(course?.original_price))}
               </p>
               <p className="text-4xl font-bold text-sky-800">
-                <span className="text-2xl font-semibold text-sky-800">
-                  {course?.discount_installments}x{' '}
-                </span>
+                <span className="text-2xl font-semibold text-sky-800">{course?.discount_installments}x </span>
                 {formatCurrency(Number(course?.discount_price))}
               </p>
               <p className="text-foreground text-xs ">NO CARTÃO DE CRÉDITO</p>
               <Button
                 asChild
-                variant={'outline'}
-                className=" w-fit h-10 text-md text-sky-800 bg-yellow-300 hover:bg-yellow-400">
-                <Link
-                  className="font-bold"
-                  to={course?.pagina_venda_embaixador}>
+                variant={"outline"}
+                className=" w-fit h-10 text-md text-sky-800 bg-yellow-300 hover:bg-yellow-400"
+              >
+                <Link className="font-bold" to={course?.pagina_venda_embaixador}>
                   REALIZAR MATRÍCULA
                 </Link>
               </Button>
@@ -165,9 +151,7 @@ export const Curso = () => {
             <Separator />
             <div className="w-full p-2 flex space-x-4 items-center justify-start">
               {<GraduationCap size={33} className="text-sky-800" />}
-              <p className="text-base text-sky-800 font-medium">
-                RECONHECIDO PELO MEC
-              </p>
+              <p className="text-base text-sky-800 font-medium">RECONHECIDO PELO MEC</p>
             </div>
             <div className="w-full p-2 flex space-x-4 items-center justify-start">
               {<Clock size={30} className="text-sky-800" />}
@@ -175,9 +159,7 @@ export const Curso = () => {
             </div>
             <div className="w-full p-2 flex space-x-4 items-center justify-start">
               {<Trophy size={30} className="text-sky-800" />}
-              <p className="text-base text-sky-800 font-medium">
-                VÁLIDO EM TODO BRASIL
-              </p>
+              <p className="text-base text-sky-800 font-medium">VÁLIDO EM TODO BRASIL</p>
             </div>
             <div className="w-full p-2 flex space-x-4 items-center justify-start">
               {<Laptop size={30} className="text-sky-800" />}
@@ -188,8 +170,7 @@ export const Curso = () => {
               <div className="space-y-2">
                 <h3 className="text-center font-semibold ">TENHO INTERESSE</h3>
                 <p className="text-center text-sm  ">
-                  <span className="font-bold ">Preencha o formulário</span> e
-                  nossa equipe entrará em contato
+                  <span className="font-bold ">Preencha o formulário</span> e nossa equipe entrará em contato
                 </p>
               </div>
               <Input
@@ -212,10 +193,7 @@ export const Curso = () => {
                 value={celular}
                 onChange={(event) => setCelular(event.target.value)}
               />
-              <Button
-                type="submit"
-                disabled={sending}
-                className="w-full bg-sky-800 hover:bg-sky-900 ">
+              <Button type="submit" disabled={sending} className="w-full bg-sky-800 hover:bg-sky-900 ">
                 ENVIAR
               </Button>
             </form>
@@ -223,5 +201,5 @@ export const Curso = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
