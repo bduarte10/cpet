cb9d856432d8aa87fd1dc5dd4da997188f386cab
Bruno <duartebruno2208@gmail.com>
2024-05-22 15:42:39 -0300

    fix: uni code

--- src/service/apiCourses.ts
+++ src/service/apiCourses.ts
@@ -1,10 +1,10 @@
-const API_URL = 'https://api.bolsamaisbrasil.com.br';
+const API_URL = "https://api.bolsamaisbrasil.com.br";
 
 export async function getCourses() {
   const data = await fetch(`${API_URL}/courses/get?level=10&school=295`)
     .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       return response.json();
     })
@@ -12,18 +12,18 @@ export async function getCourses() {
       return data;
     })
     .catch((error) => {
-      console.error('There was a problem with the fetch operation:', error);
+      console.error("There was a problem with the fetch operation:", error);
     });
 
   return data;
 }
 
 export const getCourseById = async (id: number) => {
-  const data = await fetch(`${API_URL}/scholarships/get?school=295&level=10&course=${id}&unit=7902&uid=2332
+  const data = await fetch(`${API_URL}/scholarships/get?school=295&level=10&course=${id}&unit=35390&uid=2332
   `)
     .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       return response.json();
     })
@@ -31,7 +31,7 @@ export const getCourseById = async (id: number) => {
       return data;
     })
     .catch((error) => {
-      console.error('There was a problem with the fetch operation:', error);
+      console.error("There was a problem with the fetch operation:", error);
     });
 
   return data;
